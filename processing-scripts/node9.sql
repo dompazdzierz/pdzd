b1ec8f5d339694bb9ccd65dee63eac23f57c8919
dompazdzierz <pazdzierzdominik@protonmail.com>
2022-05-30 16:56:15 +0200

    Update node9.sql

--- processing-scripts/node9.sql
+++ processing-scripts/node9.sql
@@ -12,28 +12,18 @@ SELECT
     TransactionProfitability,
     mixt1.RegionExporter AS RegionExporter,
     DevelopmentIndicator,
-    (CASE WHEN tt.row_rank_value2 < ((select count(*) from MIX_TMP1 WHERE tt.RegionExporter = RegionExporter) * 0.05)
+    (CASE WHEN (Value / (Select MAX(Value) from MIX_TMP1 WHERE mixt1.RegionExporter = RegionExporter) > 0.95)
         THEN 1
-        ELSE (CASE WHEN tt.row_rank_value2 < ((select count(*) from MIX_TMP1 WHERE tt.RegionExporter = RegionExporter) * 0.15)
+        ELSE (CASE WHEN (Value / (Select MAX(Value) from MIX_TMP1 WHERE mixt1.RegionExporter = RegionExporter) > 0.80)
             THEN 2
             ELSE 3
         END)
     END) AS AreaExportValueTier,
-    (CASE WHEN tt.row_rank_volume2 < ((select count(*) from MIX_TMP1 WHERE tt.RegionExporter = RegionExporter) * 0.05)
+    (CASE WHEN (Volume / (Select MAX(Volume) from MIX_TMP1 WHERE mixt1.RegionExporter = RegionExporter) > 0.95)
         THEN 1
-        ELSE (CASE WHEN tt.row_rank_volume2 < ((select count(*) from MIX_TMP1 WHERE tt.RegionExporter = RegionExporter) * 0.15)
+        ELSE (CASE WHEN (Volume / (Select MAX(Volume) from MIX_TMP1 WHERE mixt1.RegionExporter = RegionExporter) > 0.80)
             THEN 2
             ELSE 3
         END)
     END) AS AreaExportVolumeTier
-FROM (
-    SELECT
-        CountryExporter,
-        RegionExporter,
-        Year,
-        RANK() OVER (ORDER BY ValueSum) AS row_rank_value2,
-        RANK() OVER (ORDER BY VolumeSum) as row_rank_volume2
-    FROM MIX_TMP2
-    GROUP BY RegionExporter
-) as tt
-JOIN MIX_TMP1 mixt1 ON tt.CountryExporter = mixt1.CountryExporter AND tt.Year = mixt1.Year;
+FROM MIX_TMP1 mixt1;
