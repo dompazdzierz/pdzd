cda4a12d362954e45253f567fe81598e25e9a884
dompazdzierz <pazdzierzdominik@protonmail.com>
2022-05-30 16:14:32 +0200

    Update node9.sql

--- processing-scripts/node9.sql
+++ processing-scripts/node9.sql
@@ -1,17 +1,39 @@
 INSERT OVERWRITE
 TABLE MIX_TMP3
 SELECT
-    tt.row_hs_code,
-    (CASE WHEN tt.row_rank2 < ((select count(*) from WTD_TMP4) * 0.05)
+    mixt1.Year AS Year,
+    HsCode,
+    HsGroup,
+    CountryImporter,
+    mixt1.CountryExporter AS CountryExporter,
+    Value,
+    Volume,
+    PriceTon,
+    TransactionProfitability,
+    mixt1.RegionExporter AS RegionExporter,
+    DevelopmentIndicator,
+    (CASE WHEN tt.row_rank_value2 < ((select count(*) from MIX_TMP1 WHERE tt.RegionExporter = RegionExporter) * 0.05)
         THEN 1
-        ELSE (CASE WHEN tt.row_rank2 < ((select count(*) from WTD_TMP4) * 0.15)
+        ELSE (CASE WHEN tt.row_rank_value2 < ((select count(*) from MIX_TMP1 WHERE tt.RegionExporter = RegionExporter) * 0.15)
             THEN 2
             ELSE 3
         END)
-    END)
+    END) AS AreaExportValueTier,
+    (CASE WHEN tt.row_rank_volume2 < ((select count(*) from MIX_TMP1 WHERE tt.RegionExporter = RegionExporter) * 0.05)
+        THEN 1
+        ELSE (CASE WHEN tt.row_rank_volume2 < ((select count(*) from MIX_TMP1 WHERE tt.RegionExporter = RegionExporter) * 0.15)
+            THEN 2
+            ELSE 3
+        END)
+    END) AS AreaExportVolumeTier
 FROM (
     SELECT
-        HsCode as row_hs_code,
-        RANK() OVER (ORDER BY AvgPriceTonAllYears) as row_rank2
-    FROM WTD_TMP4
-) as tt;
+        CountryExporter,
+        RegionExporter,
+        Year,
+        RANK() OVER (ORDER BY ValueSum) AS row_rank_value2,
+        RANK() OVER (ORDER BY VolumeSum) as row_rank_volume2
+    FROM MIX_TMP2
+    GROUP BY RegionExporter
+) as tt
+JOIN MIX_TMP1 mixt1 ON tt.CountryExporter = mixt1.CountryExporter AND tt.Year = mixt1.Year;
